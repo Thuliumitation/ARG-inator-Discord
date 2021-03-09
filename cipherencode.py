3160223374b66ea2802250caad67702ef8fd2efd
Tamim Anaf <48618923+Thuliumify@users.noreply.github.com>
2021-03-09 12:49:36 +0600

    Update cipherencode.py

--- cipherencode.py
+++ cipherencode.py
@@ -33,6 +33,7 @@ class CipherEncode(commands.Cog):
             e.add_field(name="Octal (oct)", value='>encode oct <text>')
             e.add_field(name="Rot47 (r47)", value='>encode rot47 <text>')
             e.add_field(name="Monoalphabetic Substitution (mono)", value='>encode mono <text>')
+            e.add_field(name="Vigenère (v)", value=">encode vigenere <'text'> <'key'>")
             await ctx.send(embed=e)
         else:
             pass
