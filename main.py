c5b5d49ae06d91e0627a3cdbed67b464b66fd9db
Tamim Anaf <48618923+Thuliumify@users.noreply.github.com>
2021-03-09 11:43:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
-load_dotenv()
-token = os.getenv('TOKEN')
+token = os.environ('TOKEN')
 client = commands.Bot(command_prefix='>',intents=discord.Intents.all(),help_command=None)
 
 @client.event
